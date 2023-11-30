866e3caee10a0f1a5d1d49a6eea4bca6e9d89fe3
Nguyen Trung Nhan <nhanntdl98@gmail.com>
2023-11-30 11:55:00 +0700

    Update local_notification.dart

--- lib/service/local_notification.dart
+++ lib/service/local_notification.dart
@@ -1,9 +1,4 @@
-import 'dart:io';
-import 'dart:typed_data';
-
-import 'package:flutter/services.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:path_provider/path_provider.dart';
 
 class LocalNotification {
   static Future initialize(
